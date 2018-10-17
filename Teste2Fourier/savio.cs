bd358617dc31870dce873ab1bcc631cd32b8f630
lucas.veiga <lucas.veiga@timeneon.com.br>
2018-10-17 08:30:45 -0300

    create sin matrix

--- Teste2Fourier/savio.cs
+++ Teste2Fourier/savio.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Teste2Fourier
 {
@@ -15,29 +13,27 @@ namespace Teste2Fourier
         {
             CultureInfo ci = (CultureInfo)CultureInfo.CurrentCulture.Clone();
             ci.NumberFormat.CurrencyDecimalSeparator = ".";
-            string filename = @"C:\Users\lucas\source\repos\Teste2Fourier\Teste2Fourier\savio_test.txt";
-            double pdValue;
+            string filename = @"C:\workspace\FFT_BOT\Teste2Fourier\savio_test.txt";
             double mediaPeriodo = 0;
             double periodo = 30;
-
-            int indice = 1;
             int startLine = 0;
-            int lineCount = 1;
 
             List<OHLCEntity> lcoOHLCEntity = new List<OHLCEntity>();
 
-            var values = File.ReadAllLines(filename).Skip((startLine)).ToList();
+            List<string> values = File.ReadAllLines(filename).Skip((startLine)).ToList();
             foreach (string line in values)
             {
                 string[] temp = line.Split(',');
                 if (temp.Length >= 4)
                 {
-                    OHLCEntity oHLCEntity = new OHLCEntity();
-                    oHLCEntity.Data = Convert.ToDateTime(temp[0]);
-                    oHLCEntity.Open = double.Parse(temp[1], NumberStyles.Any, ci);
-                    oHLCEntity.High = double.Parse(temp[2], NumberStyles.Any, ci);
-                    oHLCEntity.Low = double.Parse(temp[3], NumberStyles.Any, ci);
-                    oHLCEntity.Close = double.Parse(temp[4], NumberStyles.Any, ci);
+                    OHLCEntity oHLCEntity = new OHLCEntity
+                    {
+                        Data = Convert.ToDateTime(temp[0]),
+                        Open = double.Parse(temp[1], NumberStyles.Any, ci),
+                        High = double.Parse(temp[2], NumberStyles.Any, ci),
+                        Low = double.Parse(temp[3], NumberStyles.Any, ci),
+                        Close = double.Parse(temp[4], NumberStyles.Any, ci)
+                    };
                     lcoOHLCEntity.Add(oHLCEntity);
                 }
             }
@@ -46,6 +42,8 @@ namespace Teste2Fourier
             mediaPeriodo = mediaPeriodo / periodo;
 
             double[] senosBase = new double[30];
+            double[,] matrixSenos = new double[10, 31];
+
             double[] senosIndice1 = new double[30];
 
             bool aindaExistePeriodo = true;
@@ -55,28 +53,37 @@ namespace Teste2Fourier
                 int skipResult = 0;
                 int takeResult = 30;
                 int countSeno = 0;
-                double indiceSeno = 1;
-                double ponto = 0.5;
+                double ponto = 0.05;
                 double ponto2 = 1.5;
                 List<OHLCEntity> OHLCPeriodo = lcoOHLCEntity.Skip(skipResult).Take(takeResult).OrderBy(x => x.Data).ToList();
 
                 if (OHLCPeriodo.Count == 0)
+                {
                     aindaExistePeriodo = false;
-
+                }
 
                 if (aindaExistePeriodo)
                 {
-                    foreach (var item in OHLCPeriodo)
+                    foreach (OHLCEntity item in OHLCPeriodo)
                     {
                         senosBase[countSeno] = item.Close - mediaPeriodo;
                         countSeno++;
                     }
 
-                    for (int i = 0; i < senosBase.Count(); i++)
+                    for (int coluna = 0; coluna < 10; coluna++)
                     {
-                        int x = i + 1;
-                        senosIndice1[i] = senosBase[i] * Math.Sin(ponto * x * ponto2 * 1);
+                        double media = 0;
+
+                        for (int linha = 0; linha < 30; linha++)
+                        {
+                            matrixSenos[coluna, linha] = senosBase[linha] * Math.Sin(ponto * (linha + 1) * ponto2 * (coluna + 1));
+                            media += matrixSenos[coluna, linha];
+                        }
+
+                        matrixSenos[coluna, 30] = media / 30;
                     }
+
+
 
                 }
             }
