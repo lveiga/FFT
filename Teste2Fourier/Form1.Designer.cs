6902f9f1c809f4f7d87dc79c53deb02397828ee8
lveigamorais <lveigamorais@gmail.com>
2018-11-04 20:00:13 -0200

    make plot wave + ohlc

--- Teste2Fourier/Form1.Designer.cs
+++ Teste2Fourier/Form1.Designer.cs
@@ -31,25 +31,26 @@
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.button2 = new System.Windows.Forms.Button();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.button1 = new System.Windows.Forms.Button();
+            this.chk8 = new System.Windows.Forms.CheckBox();
+            this.chk16 = new System.Windows.Forms.CheckBox();
+            this.chk32 = new System.Windows.Forms.CheckBox();
+            this.chk64 = new System.Windows.Forms.CheckBox();
+            this.chk128 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
             this.SuspendLayout();
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(370, 33);
+            this.button2.Location = new System.Drawing.Point(88, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -70,35 +71,26 @@
             series1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             series1.Legend = "Legend1";
             series1.Name = "Mercado";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series2.Color = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            series2.Label = "Soma";
+            series2.Legend = "Legend1";
+            series2.Name = "SomaOndas";
+            series2.XAxisType = System.Windows.Forms.DataVisualization.Charting.AxisType.Secondary;
+            series2.YAxisType = System.Windows.Forms.DataVisualization.Charting.AxisType.Secondary;
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(719, 300);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(763, 300);
             this.chart1.TabIndex = 2;
             this.chart1.Text = "chart1";
             // 
-            // chart2
-            // 
-            chartArea2.Name = "ChartArea1";
-            this.chart2.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart2.Legends.Add(legend2);
-            this.chart2.Location = new System.Drawing.Point(25, 449);
-            this.chart2.Name = "chart2";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series2.Color = System.Drawing.Color.Navy;
-            series2.Legend = "Legend1";
-            series2.Name = "SomaOndas";
-            this.chart2.Series.Add(series2);
-            this.chart2.Size = new System.Drawing.Size(719, 300);
-            this.chart2.TabIndex = 3;
-            this.chart2.Text = "chart2";
-            // 
             // chart3
             // 
-            chartArea3.Name = "ChartArea1";
-            this.chart3.ChartAreas.Add(chartArea3);
-            legend3.Name = "Legend1";
-            this.chart3.Legends.Add(legend3);
+            chartArea2.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart3.Legends.Add(legend2);
             this.chart3.Location = new System.Drawing.Point(760, 97);
             this.chart3.Name = "chart3";
             series3.ChartArea = "ChartArea1";
@@ -111,7 +103,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(617, 32);
+            this.button1.Location = new System.Drawing.Point(197, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 5;
@@ -119,31 +111,89 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // chk8
+            // 
+            this.chk8.AutoSize = true;
+            this.chk8.Location = new System.Drawing.Point(88, 62);
+            this.chk8.Name = "chk8";
+            this.chk8.Size = new System.Drawing.Size(32, 17);
+            this.chk8.TabIndex = 6;
+            this.chk8.Text = "8";
+            this.chk8.UseVisualStyleBackColor = true;
+            // 
+            // chk16
+            // 
+            this.chk16.AutoSize = true;
+            this.chk16.Location = new System.Drawing.Point(192, 62);
+            this.chk16.Name = "chk16";
+            this.chk16.Size = new System.Drawing.Size(38, 17);
+            this.chk16.TabIndex = 7;
+            this.chk16.Text = "16";
+            this.chk16.UseVisualStyleBackColor = true;
+            // 
+            // chk32
+            // 
+            this.chk32.AutoSize = true;
+            this.chk32.Location = new System.Drawing.Point(293, 62);
+            this.chk32.Name = "chk32";
+            this.chk32.Size = new System.Drawing.Size(38, 17);
+            this.chk32.TabIndex = 8;
+            this.chk32.Text = "32";
+            this.chk32.UseVisualStyleBackColor = true;
+            // 
+            // chk64
+            // 
+            this.chk64.AutoSize = true;
+            this.chk64.Location = new System.Drawing.Point(408, 62);
+            this.chk64.Name = "chk64";
+            this.chk64.Size = new System.Drawing.Size(38, 17);
+            this.chk64.TabIndex = 9;
+            this.chk64.Text = "64";
+            this.chk64.UseVisualStyleBackColor = true;
+            // 
+            // chk128
+            // 
+            this.chk128.AutoSize = true;
+            this.chk128.Location = new System.Drawing.Point(503, 62);
+            this.chk128.Name = "chk128";
+            this.chk128.Size = new System.Drawing.Size(44, 17);
+            this.chk128.TabIndex = 10;
+            this.chk128.Text = "128";
+            this.chk128.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1499, 785);
+            this.ClientSize = new System.Drawing.Size(1523, 813);
+            this.Controls.Add(this.chk128);
+            this.Controls.Add(this.chk64);
+            this.Controls.Add(this.chk32);
+            this.Controls.Add(this.chk16);
+            this.Controls.Add(this.chk8);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.chart3);
-            this.Controls.Add(this.chart2);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.button2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox chk8;
+        private System.Windows.Forms.CheckBox chk16;
+        private System.Windows.Forms.CheckBox chk32;
+        private System.Windows.Forms.CheckBox chk64;
+        private System.Windows.Forms.CheckBox chk128;
     }
 }
 
--- Teste2Fourier/Form1.cs
+++ Teste2Fourier/Form1.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Windows.Threading;
 
 
@@ -17,7 +18,7 @@ namespace Teste2Fourier
     public partial class Form1 : Form
     {
         public List<OHLCEntity> lcoOHLCEntity = new List<OHLCEntity>();
-        OndaNegocio s = new OndaNegocio(30);
+        OndaNegocio ondaNegocio = new OndaNegocio(32);
         int skip = 0;
         int take = 30;
 
@@ -25,35 +26,36 @@ namespace Teste2Fourier
         {
             InitializeComponent();
 
-            button2.Click += button2_Click;
-            button1.Click += button1_Click;
             button1.Enabled = false;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            lcoOHLCEntity = s.processar();
+            lcoOHLCEntity = ondaNegocio.processar();
 
             List<OHLCEntity> OHLCPeriodo = lcoOHLCEntity.Skip(skip).Take(take).OrderBy(x => x.Data).ToList();
 
-            s.IniciarAnalise(OHLCPeriodo, take, skip);
+            ondaNegocio.IniciarAnalise(OHLCPeriodo, take, skip);
 
-            double maxValue = s.Mercado.Max();
-            double minValue = s.Mercado.Min();
+            double maxValue = ondaNegocio.Mercado.Max();
+            double minValue = ondaNegocio.Mercado.Min();
+            double maxValue2 = ondaNegocio.SomaOndas.Max();
+            double minValue2 = ondaNegocio.SomaOndas.Min();
             
             var chart = chart1.ChartAreas[0];
             chart.AxisY.Maximum = maxValue;
             chart.AxisY.Minimum = minValue;
-            
+            chart.AxisY2.Maximum = maxValue2;
+            chart.AxisY2.Minimum = minValue2;
 
-            chart1.Series["Mercado"].Points.DataBindY(s.Mercado);
-            chart2.Series["SomaOndas"].Points.DataBindY(s.SomaOndas);
+            chart1.Series["Mercado"].Points.DataBindY(ondaNegocio.Mercado);
+            chart1.Series["SomaOndas"].Points.DataBindY(ondaNegocio.SomaOndas);
 
             for (int coluna = 0; coluna < 1; coluna++)
             {
                 for (int linha = 0; linha < 30; linha++)
                 {
-                    double mag = (1.0 / 30) * (Math.Sqrt(Math.Pow(s.complex[coluna, linha].Real, 2) + Math.Pow(s.complex[coluna, linha].Imaginary, 2)));
+                    double mag = (1.0 / 30) * (Math.Sqrt(Math.Pow(ondaNegocio.complex[coluna, linha].Real, 2) + Math.Pow(ondaNegocio.complex[coluna, linha].Imaginary, 2)));
                     chart3.Series["Frequency"].Points.AddXY(1 * linha, mag);
                 }
             }
@@ -81,28 +83,83 @@ namespace Teste2Fourier
         {
             List<OHLCEntity> OHLCPeriodo = lcoOHLCEntity.Skip(skip).Take(30).OrderBy(x => x.Data).ToList();
 
-            s.IniciarAnalise(OHLCPeriodo, 30, skip);
+            ondaNegocio.IniciarAnalise(OHLCPeriodo, 30, skip);
 
-            double maxValue = s.Mercado.Max();
-            double minValue = s.Mercado.Min();
+            double maxValue = ondaNegocio.Mercado.Max();
+            double minValue = ondaNegocio.Mercado.Min();
 
             var chart = chart1.ChartAreas[0];
             chart.AxisY.Maximum = maxValue;
             chart.AxisY.Minimum = minValue;
 
-            chart1.Series["Mercado"].Points.DataBindY(s.Mercado);
-            chart2.Series["SomaOndas"].Points.DataBindY(s.SomaOndas);
+            chart1.Series["Mercado"].Points.DataBindY(ondaNegocio.Mercado);
+            chart1.Series["SomaOndas"].Points.DataBindY(ondaNegocio.SomaOndas);
 
             for (int coluna = 0; coluna < 1; coluna++)
             {
                 for (int linha = 0; linha < 30; linha++)
                 {
-                    double mag = (1.0 / 30) * (Math.Sqrt(Math.Pow(s.complex[s.maior, linha].Real, 2) + Math.Pow(s.complex[s.maior, linha].Imaginary, 2)));
+                    double mag = (1.0 / 30) * (Math.Sqrt(Math.Pow(ondaNegocio.complex[ondaNegocio.maior, linha].Real, 2) + Math.Pow(ondaNegocio.complex[ondaNegocio.maior, linha].Imaginary, 2)));
                     chart3.Series["Frequency"].Points.AddXY(1 * linha, mag);
                 }
             }
 
             skip++;
+        }
+
+        public void CreateYAxis(Chart chart, ChartArea area, Series series, float axisOffset, float labelsSize)
+        {
+            // Create new chart area for original series
+            ChartArea areaSeries = chart.ChartAreas.Add("ChartArea_" + series.Name);
+            areaSeries.BackColor = Color.Transparent;
+            areaSeries.BorderColor = Color.Transparent;
+            areaSeries.Position.FromRectangleF(area.Position.ToRectangleF());
+            areaSeries.InnerPlotPosition.FromRectangleF(area.InnerPlotPosition.ToRectangleF());
+            areaSeries.AxisX.MajorGrid.Enabled = false;
+            areaSeries.AxisX.MajorTickMark.Enabled = false;
+            areaSeries.AxisX.LabelStyle.Enabled = false;
+            areaSeries.AxisY.MajorGrid.Enabled = false;
+            areaSeries.AxisY.MajorTickMark.Enabled = false;
+            areaSeries.AxisY.LabelStyle.Enabled = false;
+            areaSeries.AxisY.IsStartedFromZero = area.AxisY.IsStartedFromZero;
+
+
+            series.ChartArea = areaSeries.Name;
+
+            // Create new chart area for axis
+            ChartArea areaAxis = chart.ChartAreas.Add("AxisY_" + series.ChartArea);
+            areaAxis.BackColor = Color.Transparent;
+            areaAxis.BorderColor = Color.Transparent;
+            areaAxis.Position.FromRectangleF(chart.ChartAreas[series.ChartArea].Position.ToRectangleF());
+            areaAxis.InnerPlotPosition.FromRectangleF(chart.ChartAreas[series.ChartArea].InnerPlotPosition.ToRectangleF());
+
+            // Create a copy of specified series
+            Series seriesCopy = chart.Series.Add(series.Name + "_Copy");
+            seriesCopy.ChartType = series.ChartType;
+            foreach (DataPoint point in series.Points)
+            {
+                seriesCopy.Points.AddXY(point.XValue, point.YValues[0]);
+            }
+
+            // Hide copied series
+            seriesCopy.IsVisibleInLegend = false;
+            seriesCopy.Color = Color.Transparent;
+            seriesCopy.BorderColor = Color.Transparent;
+            seriesCopy.ChartArea = areaAxis.Name;
+
+            // Disable drid lines & tickmarks
+            areaAxis.AxisX.LineWidth = 0;
+            areaAxis.AxisX.MajorGrid.Enabled = false;
+            areaAxis.AxisX.MajorTickMark.Enabled = false;
+            areaAxis.AxisX.LabelStyle.Enabled = false;
+            areaAxis.AxisY.MajorGrid.Enabled = false;
+            areaAxis.AxisY.IsStartedFromZero = area.AxisY.IsStartedFromZero;
+            areaAxis.AxisY.LabelStyle.Font = area.AxisY.LabelStyle.Font;
+
+            // Adjust area position
+            areaAxis.Position.X -= axisOffset;
+            areaAxis.InnerPlotPosition.X += labelsSize;
+
         }
     }
 }
